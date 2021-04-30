c130c7da25c4ad2c7f9c52c1ba752bba483c2347
Hannah Lumapas <lumapas.h@gmail.com>
2021-04-30 15:22:19 -0400

    creates get planets and post planet endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -15,5 +15,8 @@ def create_app(test_config=None):
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
     
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
-from flask import Blueprint
+from app import db 
+from app.models.planet import Planet 
+from flask import request, Blueprint, make_response, jsonify
+
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET", "POST"])
+def handle_planets():
+    if request.method == "GET": 
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets: 
+            planets_response.append({
+                "id": planet.id, 
+                "name": planet.name, 
+                "description": planet.description
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST": 
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["title"],
+                            description=request_body["description"])
+        
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
 
