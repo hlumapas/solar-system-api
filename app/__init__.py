a64c0cf78ede09847745d922f217afcbeb01829a
mairandomness <mairakodama@gmail.com>
2021-05-03 10:28:02 -0700

    Refactor routes, so functions have one job. Add variable rules on get planet by id. Format using autopep8

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-    
+
     from app.models.planet import Planet
 
     db.init_app(app)
@@ -18,5 +19,5 @@ def create_app(test_config=None):
 
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
-    
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,59 @@
-from app import db 
-from app.models.planet import Planet 
+from app import db
+from app.models.planet import Planet
 from flask import request, Blueprint, make_response, jsonify
 
 
-planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+planets_bp = Blueprint(
+    "planets",
+    __name__,
+    url_prefix="/planets",
+    strict_slashes=False)
 
-@planets_bp.route("", methods=["GET", "POST"])
-def handle_planets():
-    if request.method == "GET": 
-        planets = Planet.query.all()
-        planets_response = []
-        for planet in planets: 
-            planets_response.append({
-                "id": planet.id, 
-                "name": planet.name, 
-                "description": planet.description
-            })
-        return jsonify(planets_response)
-    elif request.method == "POST": 
-        request_body = request.get_json()
-        new_planet = Planet(name=request_body["name"],
-                            description=request_body["description"])
-        
-        db.session.add(new_planet)
-        db.session.commit()
 
-        return make_response(f"Planet {new_planet.name} successfully created", 201)
+@planets_bp.route("", methods=["GET"])
+def list_planets():
+    planets = Planet.query.all()
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        })
+    return jsonify(planets_response)
 
-@planets_bp.route("/<planet_id>", methods=["GET"]) #, "DELETE", "PUT"
-def handle_planet(planet_id):
+
+@planets_bp.route("", methods=["POST"], strict_slashes=False)
+def add_new_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+# to implement delete, put
+
+
+@planets_bp.route("/<int:planet_id>", methods=["GET"], strict_slashes=False)
+def get_planet_by_id(planet_id):
     if request.method == "GET":
         planet = Planet.query.get(planet_id)
         if planet:
             planet_response = {
-                "id": planet.id, 
-                "name": planet.name, 
+                "id": planet.id,
+                "name": planet.name,
                 "description": planet.description
             }
-            return planet_response
+            return jsonify(planet_response)
         else:
-            return make_response(f"Planet outside of the bounds of the universe :(", 404)
+            return make_response(
+                f"Planet outside of the bounds of the universe :(", 404)
+
+
+@planets_bp.route("/<string:planet_id>", methods=["GET"], strict_slashes=False)
+def get_not_int_planet_id(planet_id):
+    return make_response(
+        f"Bad request, please enter an integer after 'planets/'", 400)
