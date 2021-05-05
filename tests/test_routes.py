2f38dcfb61ba57b0c700c33558210981130f6863
Hannah Lumapas <lumapas.h@gmail.com>
2021-05-04 21:14:10 -0400

    create test for posting a new planet and get all planets with populated db

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,6 +6,34 @@ def test_get_all_planets_with_no_records(client):
     assert response_body == []
 
 
+def test_get_all_planets_with_records(client, three_saved_planets):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == [{
+        "name": "Earth",
+        "description": "nice big rock",
+        "id": 1
+    },{
+    
+        "name": "Pluto",
+        "description": "am I a planet tho?",
+        "id": 2
+    },{
+    
+        "name": "Mars",
+        "description": "I have robots on me",
+        "id": 3
+    }]
+
+
+def test_post_new_planet(client):
+    response = client.post("/planets", json={"name":"Mercury", "description":"its color is boring gray"})
+    
+    assert response.status_code == 201
+    assert response.get_data().decode("utf-8") == "Planet Mercury successfully created" 
+
 def test_get_one_planet_planet_is_there(client, three_saved_planets):
     response = client.get("/planets/1")
     response_body = response.get_json()
